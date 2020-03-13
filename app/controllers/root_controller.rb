b4ee723688a61deb134dd6b495bf9e6f5895260c
hikaru08 <ur.mh08@gmail.com>
2020-03-13 19:33:49 +0900

    ready

---
+++ app/controllers/root_controller.rb
@@ -0,0 +1,18 @@
+class RootController < ApplicationController
+  def index
+  end
+  def company
+  end
+  def lifesupport
+  end
+  def recovery
+  end
+  def cleaning
+  end
+  def performance
+  end
+  def info
+  end
+  def plan
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'root#index'
+  get 'root/company', to: 'root#company'
+  get 'root/lifesupport', to: 'root#lifesupport'
+  get 'root/recovery', to: 'root#recovery'
+  get 'root/cleaning', to: 'root#cleaning'
+  get 'root/performance', to: 'root#performance'
+  get 'root/info', to: 'root#info'
+  get 'root/plan', to: 'root#plan'
 end
