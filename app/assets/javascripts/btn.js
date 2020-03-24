5dffedb34e3115defea6a827b1faab1efb0695ff
Syoma Iwauchi <syoma4645@gmail.com>
2020-03-24 19:06:11 +0900

    jsファイルを整理

--- app/assets/javascripts/btn.js
+++
@@ -1,6 +0,0 @@
-window.onload = function(){
-  $(document).on('click', '.footer__btn', function(){
-    $('.footer').toggleClass('hidden');
-    $('.footer__btn').toggleClass('hidden');
-  });
-};
--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -3,4 +3,11 @@ window.onload = function(){
     $('.toggle').toggleClass('active');
     $('.navi').toggleClass('active');
   });
+
+
+  // footer-btn
+  $(document).on('click', '.footer__btn', function(){
+    $('.footer').toggleClass('hidden');
+    $('.footer__btn').toggleClass('hidden');
+  });
 };
