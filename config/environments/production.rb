941a2bed02fc9de013cc46eee4538de4864de3ef
hikaru08 <ur.mh08@gmail.com>
2020-04-09 22:41:00 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,9 +100,9 @@ Rails.application.configure do
     domain: 'gmail.com',
     port:587,
     #gmailのユーザアカウント（xxxx@gmail.com)※念のため、credentials.yml.enc行き
-    user_name: Rails.application.credentials.gmail[:user],
+    user: Rails.application.credentials.gmail[:user],
     #gmail２段階認証回避のためにアプリケーションでの利用パスワードを取得、必ずcredentials.yml.endに設定を！！
-    password: Rails.application.credentials.gmail[:pass],
+    pass: Rails.application.credentials.gmail[:pass],
     #パスワードをBase64でエンコード
     authentication: :login
   }
