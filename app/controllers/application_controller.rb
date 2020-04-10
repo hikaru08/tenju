2c8a5ccc80450e67084af78002795f8258d5f5a9
hikaru08 <ur.mh08@gmail.com>
2020-04-10 11:21:29 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth, if: :production? 
-  private
-
-  def production?
-    Rails.env.production?
-  end
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username: Rails.application.credentials.basic[:user] && password: Rails.application.credentials.basic[:pass]
-    end
-  end
 
 end
