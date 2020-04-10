d3499bb5b79fcf15417c9dc59a343db6a4038d1f
hikaru08 <ur.mh08@gmail.com>
2020-04-10 11:15:26 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == Rails.application.credentials.basic[:user] && password == Rails.application.credentials.basic[:pass]
+      username: Rails.application.credentials.basic[:user] && password: Rails.application.credentials.basic[:pass]
     end
   end
 
