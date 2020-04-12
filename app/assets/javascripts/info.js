a533e23035aec7be5bdb5a202d69160ce3ea1ec8
hikaru08 <ur.mh08@gmail.com>
2020-04-12 17:04:16 +0900

    addToNewUrl_for_info

--- app/assets/javascripts/info.js
+++ app/assets/javascripts/info.js
@@ -1,10 +1,23 @@
-$(function() {
-  $('.submit-btn').prop('disabled', true);
-  $('.agree').on('click', function() {
-    if ($(this).prop('checked') == false) {
-        $('.submit-btn').prop('disabled', true);
-    } else {
-        $('.submit-btn').prop('disabled', false);
-    }
+$(document).on('turbolinks:load',function(){
+  $(function() {
+    $('.submit-btn').prop('disabled', true);
+    $('.agree').on('click', function() {
+      if ($(this).prop('checked') == false) {
+          $('.submit-btn').prop('disabled', true);
+      } else {
+          $('.submit-btn').prop('disabled', false);
+      }
+    });
   });
 });
+
+function addToNewUrl() {
+  let path = location.pathname;
+  let pattern = /^.*\/info.*$/
+
+  // URLが正しければ変更は不要
+  if(path.match(pattern)) return;
+
+  // URLを変更
+  history.replaceState('', '', `/root/info`)
+}
