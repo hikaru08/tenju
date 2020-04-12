2e00d648ef9996d0664f0ac8d92eb202ed07cf83
takuma10Mi <takumaii@yahoo.co.jp>
2020-04-12 11:19:38 +0900

    戻る実装ok

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -29,12 +29,17 @@ class RootController < ApplicationController
   end
  
   def thanks
-    # メール送信
-    @contact = Contact.new(contact_params)
-    ContactMailer.received_email(@contact).deliver
- 
-    # 完了画面を表示
-    render :action => 'thanks'
+    if params[:send]      
+      # メール送信
+      @contact = Contact.new(contact_params)
+      ContactMailer.received_email(@contact).deliver
+      # 送信が押されれば完了画面
+      render action: 'thanks'
+    elsif params[:back]
+      # 戻る画面が押されれば入力画面
+      @contact = Contact.new(contact_params)
+      render action: 'info'
+    end
   end
   def plan
   end
